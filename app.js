16fa688a2b1aaa853c4be883497285a2e4aebf2e
Sebastian Wachter <sebi.wachter@online.de>
2016-11-29 11:29:54 +0100

    download doesn't want to work the way i want it to work

--- app.js
+++ app.js
@@ -13,7 +13,35 @@ app.get('/', (req, res) => {
 });
 
 app.get('/public/saved/:fileName', (req, res) => {
-  res.download(`/public/saved/${req.params.fileName}`);
+  res.writeHead(200, {
+    'Content-Disposition': 'attachment; filename=' + req.params.fileName + '; modification-date="Wed, 12 Feb 1997 25:00:00 -0500"'
+  });
+  res.render('index');
+  /*
+
+  */
+  //res.attachment(`/public/saved/${req.params.fileName}`);
+
+  /*
+  http.get(app, (response) => {
+
+    response.pipe(res);
+  }).on('error', (err) => {
+    res.send(500, err.message);
+  });
+  */
+  /*
+  require('http').get('http://localhost:1234/', function(response) {
+    response.pipe(res);
+  }).on('error', function(err) {
+    res.send(500, err.message);
+  });
+  */
+  /*
+  response.writeHead(200, {
+    'Content-Disposition': 'attachment; filename=genome.jpeg; modification-date="Wed, 12 Feb 1997 16:29:51 -0500"'
+  });
+  */
 });
 
 io.on('connection', (socket) => {
